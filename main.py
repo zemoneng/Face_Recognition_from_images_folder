3c655b4a8e6d0eb8dada07bf8e8cefa4e7a3f190
Zemoneng <zemoneng@users.noreply.github.com>
2021-07-28 00:20:36 +0300

    добавлен парсер информации об изображениях в указанном нами каталоге добавлено создание базы картинок на SQlite с некоторыми первичными данными

--- main.py
+++ main.py
@@ -2,38 +2,67 @@ import os
 import sqlite3
 
 # Константы и параметры конфигурации:
-ALLOW_FORMATS = (".tif", ".jpeg", ".jpg")  # форматы файлов, которые мы ищем
-images_folder_path = input("Введите адрес папки с изображениями:")
-images_converted_folder_path = input("Введите адрес папки для сохранения уменьшенных копий картинок:")
+allow_formats = (".tif", ".jpeg", ".jpg")  # форматы изображений, с которыми будем работать
+import_directory = input("Введите адрес папки с изображениями:")
+output_directory = input("Введите адрес папки где будет хранится база данных и превью-картинки:")
 
-def build_index_images(images_folder_path):
-    """ Обходит папки и подпапки, находит файлы формата jpg, jpeg, tif, строит индекс:
-    image_id - идентификатов по порядку (int)
-    image_source_address - адрес и имя изначального файла (str)
-    source - источник данных (int)
-    (здесь source=0 - источник корневая папка. Будут еще: 1 - источник из интернета, 2 - подгрузка картинки вручную)
 
-    После индексации идет стадия загрузки и обработки изображений, таблица пополнится столбцами:
-    image_converted_address - адрес обработанной для распознавания картинки (str)
-    (если на фото не найдены лица, то не хранить это фото, значит адрес будет пустой)
-    image_status - статус картинки (str)
-    (Если image_status = indexed - проиндексирована, converted - скачана, обработана и загружена)
-    hash - хэш исходной картинки (str), чтобы следить не подменили ли картинку в базе
-    hash_check_status - статус после проверки хэша файла (bool), True - не именился хэш, False - изменился и надо...
-    ...заново распознать картинку.
+def build_index_images(allow_formats, import_directory, output_directory):
+    """ Находит в каталоге import_directory все файлы форматов allow_formats
+    и строит индекс в папке output_directory"""
 
-    После распознавания таблица пополнится столбцами:
-    is_any_faces - есть ли на фото лица (bool) True/False, True - нашлось хотя бы одно лицо, False - нет лиц
-    num_of_faces - кол-во лиц, найденных на фото (int)
-    """
-    for dirpath, dirnames, filenames in os.walk(images_folder_path):
-        # перебрать файлы
-        for filename in filenames:  # перебирает только файлы, отбрасывая папки
-            if os.path.splitext(filename)[1] in ALLOW_FORMATS:  # есть ли файлы выбранных нами форматов
-                print(os.path.splitext(filename)) # выводит имя и формат файла в кортеж, пример: ('0-389255', '.tif')
-                print(os.path.join(dirpath, filename))  # выводит абсолютный путь к файлу
+    conn = sqlite3.connect(output_directory + '/index.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS images(
+       image_id INT PRIMARY KEY,
+       image_name TEXT,
+       image_format TEXT,
+       image_path TEXT,
+       image_source INT,
+       image_status INT,
+       image_hash INT,
+       image_height INT,
+       image_width INT,
+       image_preview_path TEXT,
+       image_is_hash_changed INT,
+       image_num_of_faces INT,
+       image_num_of_persons INT);""")  # создаем таблицу информации об изображениях
+    conn.commit()
 
-    pass
+    cur.execute("""CREATE TABLE IF NOT EXISTS faces(
+        face_id INT PRIMARY KEY,
+        image_id INT,
+        face_coordinates TEXT,
+        face_path TEXT,
+        face_encoding TEXT,
+        face_number_of_persons_match INT);""")  # создаем таблицу информации о найденных лицах
+    conn.commit()
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS persons(
+        person_id INT PRIMARY KEY,
+        faces_id_list TEXT,
+        person_name TEXT);""")  # создаем таблицу информации о персонах
+    conn.commit()
+
+    counter = 0
+    for dirpath, dirnames, filenames in os.walk(import_directory):
+        # перебор файлов в каталоге import_directory
+        for filename in filenames:  # отбрасываем папки, оставляем только файлы
+            if os.path.splitext(filename)[1] in allow_formats:  # отбрасывает файлы, отличные от allow_formats
+                image_id = counter  # присваиваем номера изображений по порядку 0,1,2,3... и т.д.
+                image_name = os.path.splitext(filename)[0]  # имя файла без расширения
+                image_format = os.path.splitext(filename)[1]  # расширение файла, пример: ".jpg"
+                image_path = os.path.join(dirpath, filename)  # полный путь файла с названием и форматом
+                image_source = 0  # (0 - взят из базы, 1 - загружен вручную, 2 - из интернета)
+                image_status = 0  # (0 - indexed, 1 - checked faces, 2 - checked persons, 3 - changed hash)
+                image_hash = os.path.getsize(os.path.join(dirpath, filename))  # пока хэш - это размер файла
+
+                image_info = (image_id, image_name, image_format, image_path, image_source, image_status, image_hash)
+
+                cur.execute("""INSERT INTO images(image_id, image_name, image_format, image_path, image_source, 
+                    image_status, image_hash) VALUES(?, ?, ?, ?, ?, ?, ?);""", image_info)
+                conn.commit()
+                counter += 1
 
 
 def build_index_faces():
@@ -42,7 +71,6 @@ def build_index_faces():
 
 def build_index_persons():
     pass
-
 
 
 if __name__ == '__main__':
